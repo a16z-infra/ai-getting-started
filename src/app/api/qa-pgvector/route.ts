e2504546fb7c2185a542e4522b1e0aa17b3a3385
Yoko Li <ykhli1013@gmail.com>
2023-06-17 00:00:30 -0700

    test out supabase pg vector

---
+++ src/app/api/qa-pgvector/route.ts
@@ -0,0 +1,93 @@
+import { Configuration, OpenAIApi } from "openai";
+import dotenv from "dotenv";
+import { OpenAI } from "langchain/llms/openai";
+import { NextResponse } from "next/server";
+import { createClient } from "@supabase/supabase-js";
+import GPT3Tokenizer from "gpt3-tokenizer";
+
+dotenv.config({ path: `.env.local` });
+
+export async function POST(request: Request) {
+  const configuration = new Configuration({
+    apiKey: process.env.OPENAI_API_KEY,
+  });
+  const openai = new OpenAIApi(configuration);
+  const supabaseClient = createClient(
+    process.env.NEXT_PUBLIC_SUPABASE_URL!,
+    process.env.SUPABASE_SERVICE_ROLE_KEY!,
+    {
+      auth: {
+        persistSession: false,
+        autoRefreshToken: false,
+      },
+    }
+  );
+
+  const { prompt } = await request.json();
+  const input = prompt.replace(/\n/g, " ");
+
+  // Generate a one-time embedding for the query itself
+  const embeddingResponse = await openai.createEmbedding({
+    model: "text-embedding-ada-002",
+    input,
+  });
+
+  const [{ embedding }] = embeddingResponse.data.data;
+  // In production we should handle possible errors
+  const { data: documents } = await supabaseClient.rpc("match_documents", {
+    query_embedding: embedding,
+    match_threshold: 0.78, // Choose an appropriate threshold for your data
+    match_count: 10, // Choose the number of matches
+  });
+
+  const tokenizer = new GPT3Tokenizer({ type: "gpt3" });
+  let tokenCount = 0;
+  let contextText = "";
+
+  // Concat matched documents
+  for (let i = 0; i < documents.length; i++) {
+    const document = documents[i];
+    const content = document.content;
+    const encoded = tokenizer.encode(content);
+    tokenCount += encoded.text.length;
+
+    // Limit context to max 1500 tokens (configurable)
+    if (tokenCount > 1500) {
+      break;
+    }
+
+    contextText += `${content.trim()}\n---\n`;
+  }
+
+  const answerPrompt = `${`
+  Given the following sections from blogs, answer the question using only that information,
+  outputted in markdown format. If you are unsure and the answer
+  is not explicitly written in the documentation, say
+  "Sorry, I don't know how to help with that."`}
+
+  Context sections:
+  ${contextText}
+
+  Question: """
+  ${prompt}
+  """
+
+  Answer as markdown (including related code snippets if available):
+`;
+
+  const completionResponse = await openai.createCompletion({
+    model: "text-davinci-003",
+    prompt: answerPrompt,
+    max_tokens: 512, // Choose the max allowed tokens in completion
+    temperature: 0, // Set to 0 for deterministic results
+  });
+
+  const {
+    id,
+    choices: [{ text }],
+  } = completionResponse.data;
+
+  const response = completionResponse.data;
+  console.log(response);
+  return NextResponse.json(response);
+}
---
+++ src/scripts/indexBlogsPgVector.mjs
@@ -0,0 +1,47 @@
+import dotenv from "dotenv";
+import { Configuration, OpenAIApi } from "openai";
+import { createClient } from "@supabase/supabase-js";
+import fs from "fs";
+import path from "path";
+
+dotenv.config({ path: `.env.local` });
+
+const configuration = new Configuration({ apiKey: process.env.OPENAI_API_KEY });
+const openai = new OpenAIApi(configuration);
+const supabaseClient = createClient(
+  process.env.NEXT_PUBLIC_SUPABASE_URL,
+  process.env.SUPABASE_SERVICE_ROLE_KEY,
+  {
+    auth: {
+      persistSession: false,
+      autoRefreshToken: false,
+    },
+  }
+);
+
+const fileNames = fs.readdirSync("blogs");
+const docs = fileNames.map((fileName) => {
+  const filePath = path.join("blogs", fileName);
+  const fileContent = fs.readFileSync(filePath, "utf8");
+  return fileContent;
+});
+console.log(docs.length);
+
+const chunkSize = 1000; // Adjust the chunk size as per your needs
+
+for (const doc of docs) {
+  const chunks = doc.match(new RegExp(`.{1,${chunkSize}}`, "g"));
+  const embeddings = [];
+  for (const chunk of chunks) {
+    const input = chunk.replace(/\n/g, " ");
+    const embeddingResponse = await openai.createEmbedding({
+      model: "text-embedding-ada-002",
+      input,
+    });
+    const [{ embedding }] = embeddingResponse.data.data;
+    const supabaseResponse = await supabaseClient.from("documents").insert({
+      content: chunk,
+      embedding: embedding,
+    });
+  }
+}
