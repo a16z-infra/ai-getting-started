923b4f48e64dbbae4b562cddbf4078bfcbb90257
Yoko Li <ykhli1013@gmail.com>
2023-06-20 12:52:56 -0700

    add new package

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,7 +1,5 @@
 import { authMiddleware } from "@clerk/nextjs";
-export default authMiddleware({
-  publicRoutes: ["/", "/api(.*)"],
-});
+export default authMiddleware();
 
 export const config = {
   matcher: ["/((?!.*\\..*|_next).*)", "/", "/(api|trpc)(.*)"],
