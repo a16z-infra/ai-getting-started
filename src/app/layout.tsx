f682fa7af32f5576b44d1efe5a6d66290a6aaf15
Yoko Li <ykhli1013@gmail.com>
2023-10-27 16:55:37 -0700

    remove Clerk

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,5 @@
 import "./globals.css";
 import { Inter } from "next/font/google";
-import { ClerkProvider } from "@clerk/nextjs";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -15,10 +14,8 @@ export default function RootLayout({
   children: React.ReactNode;
 }) {
   return (
-    <ClerkProvider>
-      <html lang="en">
-        <body className={inter.className}>{children}</body>
-      </html>
-    </ClerkProvider>
+    <html lang="en">
+      <body className={inter.className}>{children}</body>
+    </html>
   );
 }
--- src/app/sign-in/[[...sign-in]]/page.tsx
+++ src/app/sign-in/[[...sign-in]]/page.tsx
@@ -1,5 +1,3 @@
-import { SignIn } from "@clerk/nextjs";
-
 export default function Page() {
-  return <div className="min-h-screen flex items-center justify-center"><SignIn /></div>;
+  return <div className="min-h-screen flex items-center justify-center"></div>;
 }
